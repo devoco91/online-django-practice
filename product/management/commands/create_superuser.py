070584546e830b70bfc9ff1341c61fded87a0c44
David <ashuaib1991@gmail.com>
2025-05-18 20:58:59 +0100

    removed

--- product/management/commands/create_superuser.py
+++ product/management/commands/create_superuser.py
@@ -1,21 +1,21 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError, ProgrammingError
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth import get_user_model
+# from django.db.utils import OperationalError, ProgrammingError
 
-class Command(BaseCommand):
-    help = "Create a superuser automatically"
+# class Command(BaseCommand):
+#     help = "Create a superuser automatically"
 
-    def handle(self, *args, **kwargs):
-        User = get_user_model()
-        username = "Devoco"
-        email = "david@gmail.com"
-        password = "Official@lasop1"
+#     def handle(self, *args, **kwargs):
+#         User = get_user_model()
+#         username = "Devoco"
+#         email = "david@gmail.com"
+#         password = "Official@lasop1"
 
-        try:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username=username, email=email, password=password)
-                self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created successfully!"))
-            else:
-                self.stdout.write(self.style.WARNING(f"Superuser '{username}' already exists."))
-        except (OperationalError, ProgrammingError) as e:
-            self.stderr.write(self.style.ERROR(f"Database not ready: {e}"))
+#         try:
+#             if not User.objects.filter(username=username).exists():
+#                 User.objects.create_superuser(username=username, email=email, password=password)
+#                 self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created successfully!"))
+#             else:
+#                 self.stdout.write(self.style.WARNING(f"Superuser '{username}' already exists."))
+#         except (OperationalError, ProgrammingError) as e:
+#             self.stderr.write(self.style.ERROR(f"Database not ready: {e}"))
